210f345d9b367233bad7ebf297b40b6b6c4dfc0e
Marcel Rico <personal.mr4@gmail.com>
2019-11-24 17:42:32 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,9 +6,10 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  array.length.times do |index,value|
-    
-    
+  max_val = 0
+  array.length.times do |index|
+    max_val = array[index] if array[index] > max_val
+  end
 end
 
 def find_min_value(array)
