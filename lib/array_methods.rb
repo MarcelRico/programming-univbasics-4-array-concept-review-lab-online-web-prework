f65285480d8dca87959ef384da243158452e4562
Marcel Rico <personal.mr4@gmail.com>
2019-11-24 17:37:38 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,5 @@
 def find_element_index(array, value_to_find)
-  counter=0
-  array.length.times |index|
-    if array[counter] == value_to_find
-      return index
-    counter+=1
-  end
-  return nil;
+  
 end
 
 def find_max_value(array)
